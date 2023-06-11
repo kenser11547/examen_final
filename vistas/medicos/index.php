16ffbe6a1c5d2b7cb1036b88817f09126c728701
kenser <koffy140221@gmail.com>
2023-06-10 20:27:30 -0600

    crear vista de medicos

---
+++ vistas/medicos/index.php
@@ -0,0 +1,33 @@
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Formulario de ingreso de Medicos</h1>
+        <div class="row justify-content-center">
+            <form action="/final_caaljuc/controladores/medicos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                    <label for="medico_nombre">Nombre del medico</label>
+                        <input type="text" name="medico_nombre" id="medico_nombre" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="medico_especialidad">Especialidad</label>
+                        <input type="number" name="medico_especialidad" id="medico_especialidad" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="medico_clinica">Clinica</label>
+                        <input type="number" name="medico_clinica" id="medico_clinica" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php include_once '../../includes/footer.php'?>
--- vistas/medicos/modificar.php
+++ vistas/medicos/modificar.php
@@ -1,9 +1,9 @@
 <?php
-require '../../modelos/Paciente.php';
+require '../../modelos/Medico.php';
     try {
-        $paciente = new Paciente($_GET);
+        $medico = new Medico($_GET);
 
-        $pacientes = $paciente->buscar();
+        $medicos = $medico->buscar();
        
     } catch (PDOException $e) {
         $error = $e->getMessage();
@@ -15,18 +15,24 @@ require '../../modelos/Paciente.php';
     <div class="container">
         <h1 class="text-center">Modificar pacientes</h1>
         <div class="row justify-content-center">
-            <form action="/final_caaljuc/controladores/pacientes/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/final_caaljuc/controladores/medicos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <input type="hidden" name="medico_id">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="paciente_nombre">Nombre del paciente</label>
-                        <input type="text" name="paciente_nombre" id="paciente_nombre" class="form-control" required>
+                        <label for="medico_nombre">Nombre del medico</label>
+                        <input type="text" name="medico_nombre" id="medico_nombre" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="paciente_nit">Nit del paciente</label>
-                        <input type="text" name="paciente_nit" id="paciente_nit" class="form-control" required>
+                        <label for="medico_especialidad">Especialidad</label>
+                        <input type="text" name="medico_especialidad" id="medico_especialidad" class="form-control" required>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="medico_clinica">Clinica</label>
+                        <input type="text" name="medico_clinica" id="medico_clinica" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
