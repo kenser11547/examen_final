8b3920003dc505d8011068d8678acdcfa4976c6e
kenser <koffy140221@gmail.com>
2023-06-10 19:05:18 -0600

    cambiar nombre base de datos

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -5,7 +5,7 @@ abstract class Conexion{
     private static function conectar(){
         try{
             //CONEXION A LA BD DE INFORMIX EN DOCKER 
-            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=hospital; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
             // DEFINIR EL MANEJO DE EXCEPCIONES
             self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // echo "CONECTADO";
