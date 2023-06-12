0821804bf3364e7db088bd2757da82e2dff97d9c
kenser <koffy140221@gmail.com>
2023-06-12 13:30:40 -0600

    crear modificacion de los pacientes

--- vistas/pacientes/modificar.php
+++ vistas/pacientes/modificar.php
@@ -1,10 +1,12 @@
 <?php
 require '../../modelos/Paciente.php';
     try {
-        $paciente = new Paciente($_GET);
+        if(isset($_GET['paciente_id']) && $_GET['paciente_id'] != ''){
 
-        $pacientes = $paciente->buscar();
-       
+            $paciente_id = $_GET['paciente_id'];
+            $paciente = new Paciente(["paciente_id" => $paciente_id]);
+            $pacientes = $paciente->buscar();
+        }
     } catch (PDOException $e) {
         $error = $e->getMessage();
     } catch (Exception $e2){
@@ -18,7 +20,7 @@ require '../../modelos/Paciente.php';
         <h1 class="text-center">Modificar pacientes</h1>
         <div class="row justify-content-center">
             <form action="/final_caaljuc/controladores/pacientes/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
-                <input type="hidden" name="medico_id">
+                <input type="hidden" name="paciente_id" value="<?= $pacientes[0]['PACIENTE_ID'] ?>">
                 <div class="row mb-3">
                     <div class="col">
                         <label for="paciente_nombre">Nombre del paciente</label>
